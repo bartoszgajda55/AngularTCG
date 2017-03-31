3bcd78f72e45b6bb6ebe93c3546c6e80210075ed
Bartosz <bartek.g55@gmail.com>
2017-03-31 23:19:59 +0100

    Recipe list polished.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://www.bbcgoodfood.com/sites/default/files/styles/push_item/public/recipe_images/recipe-image-legacy-id--1090476_11.jpg?itok=PMFhUnws')
+    new Recipe('A Test Recipe', 'This is a test', 'https://www.bbcgoodfood.com/sites/default/files/styles/push_item/public/recipe_images/recipe-image-legacy-id--1090476_11.jpg?itok=PMFhUnws'),
+    new Recipe('A Test 2 Recipe', 'This is a test 2', 'https://www.bbcgoodfood.com/sites/default/files/styles/push_item/public/recipe_images/recipe-image-legacy-id--1090476_11.jpg?itok=PMFhUnws'),
   ];
 
   constructor() { }
