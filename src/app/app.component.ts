be884b960beef8d78a2894102fb5dab3e229fc9d
Bartosz <bartek.g55@gmail.com>
2017-04-02 22:11:25 +0100

    Event emitter added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  numberToEmit: number = 0;
+  @Output()
+  numberEmitted = new EventEmitter<{number: number}>();
+  timer: any;
 
+  onGameStart() {
+    this.timer = setInterval(() => {
+      this.numberEmitted.emit({number: this.numberToEmit++});
+      console.log(this.numberToEmit);
+    }, 1000);
+  }
+
+  onGameStop() {
+    clearInterval(this.timer);
+  }
 }
