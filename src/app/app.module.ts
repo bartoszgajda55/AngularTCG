2b427877d3959d34ef30d06237f038b32d0dcf2d
Bartosz <bartek.g55@gmail.com>
2017-04-03 22:12:28 +0100

    Cleaned up master branch.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
-import {LoggingService} from "./logging.service";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [
-    LoggingService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
