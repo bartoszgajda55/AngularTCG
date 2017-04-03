ea405e11f20598720e99fac9c651635c7cf94a81
Bartosz <bartek.g55@gmail.com>
2017-04-03 16:40:53 +0100

    Using renderer in directive.

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,16 @@
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+
+  constructor(
+    private renderer: Renderer2,
+    private elRef: ElementRef
+  ) { }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+}
