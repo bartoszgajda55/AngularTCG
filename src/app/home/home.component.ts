33192b28a239fd12d3708bd93f135227c50c4be6
Bartosz <bartek.g55@gmail.com>
2017-04-06 19:21:22 +0100

    Custom Observable.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from "rxjs";
+import {Observable} from "rxjs/Observable";
+import {Observer} from "rxjs/Observer";
 import 'rxjs/Rx';
 
 @Component({
@@ -12,10 +13,38 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      } ,2000);
+      setTimeout(() => {
+        observer.next('second package');
+      } ,4000);
+      setTimeout(() => {
+        // observer.error('this is error');
+        observer.complete();
+      } ,5000);
+      setTimeout(() => {
+        observer.error('third package');
+        observer.complete();
+      } ,6000);
+    });
+    myObservable.subscribe(
+      (data: string) => {
+        console.log('data: ' + data);
+      },
+      (error: string) => {
+        console.log('error: ' + error);
+      },
+      () => {
+        console.log('completed');
       }
     );
   }
