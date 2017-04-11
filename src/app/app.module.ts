127f0ebb64e1ab4bc9038196a9880982939c0035
Bartosz <bartek.g55@gmail.com>
2017-04-11 19:53:10 +0100

    Cleaned up master branch.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import {SharedModule} from "./shared/shared.module";
-import {ShoppingListModule} from "./shopping-list/shopping-list.module";
-import {AuthModule} from "./auth/auth.module";
-import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -15,13 +11,10 @@ import {CoreModule} from "./core/core.module";
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    AppRoutingModule,
-    SharedModule,
-    ShoppingListModule,
-    AuthModule,
-    CoreModule
+    FormsModule,
+    HttpModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
