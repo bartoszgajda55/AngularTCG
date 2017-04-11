26515646a0998375620cbe392f893b8d99ec5cc5
Bartosz <bartek.g55@gmail.com>
2017-04-11 20:27:30 +0100

    Transitions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(300))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
@@ -16,6 +32,7 @@ export class AppComponent {
   }
 
   onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
   }
 
   onShrink() {
