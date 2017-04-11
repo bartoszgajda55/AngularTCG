396ee1d3c92308aa6fe35132fd6694507bdd6a92
Bartosz <bartek.g55@gmail.com>
2017-04-11 13:16:52 +0100

    Signing users in.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,12 @@
+import * as firebase from 'firebase';
+
 export class AuthService {
   signupUser(email: string, password: string) {
-
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then().catch(
+      (error) => {
+        console.log(error);
+      }
+    )
   }
 }
