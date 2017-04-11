2ab8ea0c0656a669873c0f6fc15488058563e03a
Bartosz <bartek.g55@gmail.com>
2017-04-11 13:31:04 +0100

    Signing users in (not up).

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,4 +9,17 @@ export class AuthService {
       }
     )
   }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        (response) => {
+          console.log(response);
+        }
+      ).catch(
+        (error) => {
+          console.log(error);
+        }
+    );
+  }
 }
